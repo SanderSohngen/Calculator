892b2eb27c03d6bab71232f8941abc63ab629805
sander <sander.sohngen@usp.br>
2021-11-30 12:20:29 -0300

    finished all but calculateResult functions

--- script.js
+++ script.js
@@ -12,7 +12,28 @@ function subtractNumbers(a, b) {return a - b; }
 function multiplyNumbers(a, b) { return a * b; }
 function divideNumbers(a, b) { return a / b; }
 
+let result = document.querySelector("#result");
 
+const buttons = {
+    clear: clearAll,
+    backspace: deleteLast,
+    // "=" : calculateResult
+};
+const keys = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "*", "/", "."];
+keys.forEach(key => buttons[key] = inputKey);
+
+
+function clearAll(keyID) { result.textContent = 0}
+function deleteLast(keyID) { result.textContent = result.textContent.split("").slice(0, -1).join(""); }
+function inputKey(keyID) { 
+    result.textContent = result.textContent == 0 ? keyID : result.textContent + keyID;
+}
+
+const calculator = document.querySelector("#calculator");
+calculator.addEventListener("click", event => {
+    const keyID = event.target.id;
+    buttons[keyID](keyID);
+})
 
 
 //TODO: use reduce when evaluation expression
